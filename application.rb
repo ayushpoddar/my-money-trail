3f70421f7d8d3b4c275e686de9a53bef5d417bcd
Ayush Poddar <ayush.mail.id@gmail.com>
2021-08-26 00:40:01 +0530

    zeitwerk reloading; use module in application

--- application.rb
+++ application.rb
@@ -2,20 +2,37 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 
 
-require "zeitwerk"
+module MyMoneyTrail
+  require "zeitwerk"
 
-loader = Zeitwerk::Loader.new
-loader.push_dir "#{__dir__}/models"
-loader.setup
+  @loader = Zeitwerk::Loader.new
+  @loader.push_dir "#{__dir__}/lib/models"
+  @loader.enable_reloading
+  @loader.setup
+
+  def reload!
+    @loader.reload
+  end
 
 
-require "active_record"
-require "yaml"
+  require "active_record"
+  require "yaml"
 
-def run
-  db_config = YAML::load(File.open("config/database.yml"))
-  ActiveRecord::Base.establish_connection(db_config)
-  yield
-  ActiveRecord::Base.remove_connection
-  puts "Bye"
+  def run
+    logger = Logger.new(STDOUT)
+    # logger.level = Logger::WARN
+
+    db_config = YAML::load(File.open("config/database.yml"))
+    ActiveRecord::Base.establish_connection(db_config)
+    ActiveRecord::Base.logger = logger
+
+    yield
+
+    ActiveRecord::Base.remove_connection
+    logger.info("Bye")
+    logger.close
+  end
+
+
+  module_function :run, :reload!
 end
