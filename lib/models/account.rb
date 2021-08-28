1e7e67e9a658935cdc05bfe1ceab3d7c6ee06ec3
Ayush Poddar <ayush.mail.id@gmail.com>
2021-08-28 21:35:05 +0530

    move to model

--- lib/models/account.rb
+++ lib/models/account.rb
@@ -2,4 +2,13 @@ class Account < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :initial_balance, presence: true, numericality: true
+
+  def add(params)
+    account = ::Account.new(params)
+    if account.save
+      return_success
+    else
+      return_error
+    end
+  end
 end
--- lib/models/application_record.rb
+++ lib/models/application_record.rb
@@ -4,4 +4,18 @@ require "active_record"
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def return_success
+    OpenStruct.new(success?: true, self.class.name.downcase => self)
+  end
+
+  def return_error(error_messages: [])
+    error_messages = self.errors.full_messages if error_messages.blank?
+
+    OpenStruct.new(
+      success?: false,
+      self.class.name.downcase => object,
+      errors: error_messages
+    )
+  end
 end
