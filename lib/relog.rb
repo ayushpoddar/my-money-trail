b4429de1788267db547e9e748ae6b8526aa2bdc2
Ayush Poddar <ayush.mail.id@gmail.com>
2021-11-18 13:45:37 +0530

    log in stdout for development mode

--- lib/relog.rb
+++ lib/relog.rb
@@ -7,9 +7,24 @@ module Relog
   def with_logging
     raise ArgumentError, "Providing a block is mandatory" unless block_given?
 
-    @logger = Logger.new("log/development.log")
+    create_logger
     @logger.info("STARTING APPLICATION IN #{Setup.env} ENVIRONMENT")
     yield(@logger)
     @logger.close
   end
+
+  private
+
+  def create_logger
+    return if defined?(@logger)
+    environment = Setup.env
+    filename = "log/#{environment}.log"
+
+    @logger = Logger.new(filename)
+    if environment.development?
+      stdout_logger = Logger.new(STDOUT)
+      @logger.extend ActiveSupport::Logger.broadcast(stdout_logger)
+    end
+  end
+
 end
