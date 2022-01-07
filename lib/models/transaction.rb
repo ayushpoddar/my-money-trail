3aff067c653331da556d78ddabee3442748cb7c8
Ayush Poddar <ayush.mail.id@gmail.com>
2022-01-07 13:59:19 +0530

    fix comments

--- lib/models/transaction.rb
+++ lib/models/transaction.rb
@@ -10,9 +10,9 @@ require_relative "helpers/enumable"
 
 class Transaction < ApplicationRecord
   # The meaning of each direction are:
-  # in: Money coming in; I am earning
-  # out: Money going out; I am expending
-  # transfer: Money being transferred from one account to other
+  ### in: Money coming in; I am earning
+  ### out: Money going out; I am expending
+  ### transfer: Money being transferred from one account to other
   module Direction
     EXPENSE  = "expense"
     INCOME   = "income"
@@ -21,11 +21,6 @@ class Transaction < ApplicationRecord
 
   extend Enumable
 
-  # enum direction: Direction.constants.map { |c|
-  #   value = Direction.const_get(c)
-  #   [value, value]
-  # }.to_h
-  #
   enumify :direction, Direction
 
   # Validations
